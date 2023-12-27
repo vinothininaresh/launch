4794fa15b0a6c375bb9465a3d63e9bd4ca9cd899
vinothininaresh <vinothininaresh1@gmail.com>
2023-12-27 12:33:39 +0530

    launch

---
+++ launch2.0/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './Pages/Home/Nav/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      
+    </div>
+  );
+}
+
+export default App;
